22fe29dc731ab09de6e211346d129aa5ebd98b0f
Qasim Farooq <qfarooq100@gmail.com>
2024-10-15 15:04:12 +0100

    Max function added
    Added the max function java file.

---
+++ max.java
@@ -0,0 +1,13 @@
+/********************************
+Developer: Muhammed Qasim Farooq
+University ID: 230112900
+Function: This function takes two inputs as integers and returns the minmum
+********************************/
+
+public int max(int a, int b){
+    if(a< b){
+        return b;
+    }else{
+        return a;
+    }
+}
