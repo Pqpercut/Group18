09c99008b14d2b5c162c0de39a931334886b238f
Qasim Farooq <qfarooq100@gmail.com>
2024-10-15 15:31:10 +0100

    Added max to Maths
    File remved, function added - Qasim

--- Maths.java
+++ Maths.java
@@ -4,5 +4,19 @@ public class Maths {
             return a-b;
      }
 
+        /********************************
+    Developer: Muhammed Qasim Farooq
+    University ID: 230112900
+    Function: This function takes two inputs as integers and returns the minmum
+    ********************************/
+
+    public int max(int a, int b){
+        if(a< b){
+            return b;
+        }else{
+            return a;
+        }
+}
+
     }
     
