b6bcc0b7198bad7210a0b2e58ab4d1b8b62d1e07
Az <azaan.mudassar@outlook.com>
2024-10-15 15:32:47 +0100

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,3 +1,9 @@
-public int multi (int a, int b) {
-    System.out.println(a * b);
+/********************************
+Developer: Azaan Mudassar
+University ID: 230098501
+Function: This function takes two inputs as integers and returns the sum
+********************************/
+
+public int multi(int a, int b) {
+   System.out.println(a * b); 
 }
