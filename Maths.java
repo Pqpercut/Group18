5728eb3b0130749e7c59dea881e67ccd26677e96
SakiWolf921 <sk.eduwork@gmail.com>
2024-10-15 15:36:02 +0100

    added divide to main

--- Maths.java
+++ Maths.java
@@ -16,7 +16,18 @@ public class Maths {
         }else{
             return a;
         }
-}
+     }
 
-    }
+     /***
+     developer: Sakina Hasnain Khaki
+     uni ID: 230111143
+     function: takes two int inputs a and b, divides a by b
+     ***/
+     public int divide(int a, int b) {
+     	int out = a / b;
+     	return(out);
+     }
+
+
+}
     
