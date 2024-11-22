90c837d6251665e210f3cd97269b7b251bd40bd7
Adam Ahmed <adamahmed2002@icloud.com>
2024-11-22 15:05:00 +0000

    Creation of IMS Product List Page
    Created the view, url routes and placeholder template for the Inventory Management Systems Product Page

--- hatsforcats/ecommerceapp/views.py
+++ hatsforcats/ecommerceapp/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from .models import *
+
+class InventoryProductListView (ListView):
+    model = Product
+    template_name = "inventory-management/product_list.html"  
+    context_object_name = "products"  
+
+# class InventoryProductDetailView (UpdateView):
+
+# class InventoryProductDeleteView (DeleteView):
--- hatsforcats/hatsforcats/urls.py
+++ hatsforcats/hatsforcats/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ecommerceapp.views import InventoryProductListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("inventory-management/products/", InventoryProductListView.as_view(), name="IMS - Product List"),
 ]
