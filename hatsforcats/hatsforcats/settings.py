40f4ad0d7a7e69fedc37fa30f4c19f4f1801d9c6
Qasim Farooq <qfarooq100@gmail.com>
2024-12-06 20:23:33 +0000

    Global searchbar in Header
    Implemented a global searchbar in the header. Also included datalists as part of the search bar.

--- hatsforcats/hatsforcats/settings.py
+++ hatsforcats/hatsforcats/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'ecommerceapp.context_processor.global_search_bar'
             ],
         },
     },
