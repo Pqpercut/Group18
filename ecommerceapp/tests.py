5134d6de7d4d5e1b218f0161b3bfdd8acaf64f7a
SakiWolf921 <sk.eduwork@gmail.com>
2025-02-15 22:33:18 +0000

    variantDisplay unit tests done
    (other than checking if correct img to correct item)

--- ecommerceapp/tests.py
+++ ecommerceapp/tests.py
@@ -1,6 +1,101 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from ecommerceapp.models import Basket, Product, BasketItem
+from ecommerceapp.models import Basket, Product, BasketItem, ProductVariant
+
+
+class variantDisplayTest(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name="Product", description="test product 1")
+
+        self.product1 = ProductVariant.objects.create(productID=self.product, size="M", colour="red", price=12, stocklevel=22)
+        self.product2 = ProductVariant.objects.create(productID=self.product, size="XL", colour="pink", price=12, stocklevel=15)
+ 
+    #correct product acquired from productid
+    def test_productid(self):
+        prod = Product.objects.get(id=self.product1.id)
+        self.assertEqual(prod.name, "Product", "wrong product!")
+
+    #all variants of product are correct
+    def test_variants_of_products(self):
+        variants = ProductVariant.objects.filter(productID=self.product.id)
+        self.assertEqual(variants.count(), 2, "wrong num variants!")
+
+    #colours map accurate to whats in stock 
+    def test_colour_map(self):
+        #expected 
+        expected = {
+            "red" : ["M"],
+            "pink" : ["XL"]
+        }
+
+        #get variants
+        variants = ProductVariant.objects.filter(productID=self.product.id)
+
+        #actual mapping 
+        actual = {}
+        for i in variants:
+            actual.setdefault(i.colour, []).append(i.size)
+
+        self.assertEqual(expected, actual, "colour mapping is wrong!")
+
+    #size map accuracy
+    def test_size_map(self):
+        #expected 
+        expected = {
+            "M" : ["red"],
+            "XL" : ["pink"]
+        }
+
+        #get variants
+        variants = ProductVariant.objects.filter(productID=self.product.id)
+
+        #actual mapping 
+        actual = {}
+        for i in variants:
+            actual.setdefault(i.size, []).append(i.colour)
+
+        self.assertEqual(expected, actual, "size mapping is wrong!")
+
+
+    #picking colour updates sizes
+    def test_colour_updates_size(self):
+        col = "red" 
+        sizes = []
+
+        variants = ProductVariant.objects.filter(productID=self.product.id, colour=col)
+
+        for i in variants: 
+            sizes.append(i.size)
+
+        self.assertEqual(sizes, ["M"], "returned "+str(sizes))
+
+
+    #picking size updates colour
+    def test_size_updates_colour(self):
+        size = "XL" 
+        cols = []
+
+        variants = ProductVariant.objects.filter(productID=self.product.id, size=size)
+
+        for i in variants: 
+            cols.append(i.colour)
+
+        self.assertEqual(cols, ["pink"], "returned "+str(cols)) 
+
+
+    #nonexistent item is not recognised
+    def test_nonexistent_item(self):
+        size = "S"
+        colour = "green"
+
+        variant = ProductVariant.objects.filter(productID=self.product.id, colour=colour, size=size).first()
+    
+        self.assertIsNone(variant, "item does not exist")
+
+    #test if correct img to name 
+    
+
+
 
 class BasketTestCase(TestCase):
     def setUp(self):
@@ -30,7 +125,6 @@ class BasketTestCase(TestCase):
 
     def test_add_product_negative_quantity(self):
         #adding w negative quantity 
-        #form wont submit if its negative anyway so do we need this test? 
         with self.assertRaises(ValueError):
             self.basket.add_product(self.product1, -1)
 
@@ -44,6 +138,8 @@ class BasketTestCase(TestCase):
         #removing non existent product from absket 
         with self.assertRaises(BasketItem.DoesNotExist):
             self.basket.remove_product(self.product2)  
+
+    #nonexistent item not added to basket, notifies user
 
     def test_update_quantity(self):
         #update quantity of existing product 
