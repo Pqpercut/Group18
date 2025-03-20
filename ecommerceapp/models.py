75c0c6f3e50b838e8d597087ec89031d468a168d
SakiWolf921 <sk.eduwork@gmail.com>
2025-03-20 13:39:40 +0000

    fixed some bugs in basket tests

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -114,9 +114,5 @@ class Wishlists (models.Model):
 # Model created by: Adam    
     userID = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'wishList')
     name = models.CharField(max_length=50, default="Wishlist #")
-    
-class BasketItem(models.Model):
-    basketID = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='wishListItem')
-    productID = models.ForeignKey(Product, on_delete=models.CASCADE, related_name = 'wishlist') 
-    variantID = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, related_name='wishListItem')
-    
+
+
